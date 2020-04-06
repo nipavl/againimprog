837bfb60861e43e1eb3d2286aea98d3b0ab97469
Nicole P <n1kolay21@yandex.ru>
2020-04-06 18:32:35 +0300

    again im programmer

---
+++ 1stNet_project/1stNet_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1stNet_project.Startup))]
+namespace _1stNet_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
